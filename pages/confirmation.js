63ec03b0477128f1d06ea6c840ef7bfc9d35b58f
leon-do <leondo123@gmail.com>
2020-07-04 13:39:48 -0400

    lint hook

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function Confirmation() {
   return (
     <div className="container">
--- pages/pay.js
+++ pages/pay.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function Pay() {
   return (
     <div className="container">
