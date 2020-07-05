300c87f9bbe329e39fb72542d3fed26e3f8db3e1
leon-do <leondo123@gmail.com>
2020-07-05 12:49:25 -0400

    add /test page

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SendETH from "../components/SendETH";
+import SendERC20 from "../components/SendERC20";
+
+export default function Test() {
+  return (
+    <>
+      <SendETH />
+      <SendERC20 />
+    </>
+  );
+}
