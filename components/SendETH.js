f499cfd22d68c6ea00da27e74bf2817e228e7f0f
leon-do <leondo123@gmail.com>
2020-07-04 21:34:55 -0400

    lint-staged

--- components/SendETH.js
+++ components/SendETH.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Web3 from "web3";
 import getNetwork from "../lib/getNetwork";
-import getEtherscanLink from '../lib/getEtherscanLink'
+import getEtherscanLink from "../lib/getEtherscanLink";
 
 export default function Confirmation() {
   const handleSend = async () => {
     const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-		const network = getNetwork(web3);
+    const network = getNetwork(web3);
     const fromAddress = (await web3.eth.getAccounts())[0];
     const toAddress = "0xc49a7E03d79d3eEFb09920263a42D33B88dA9250";
     const amount = 0.001;
@@ -18,9 +18,9 @@ export default function Confirmation() {
         value: web3.utils.toWei(amount.toString(), "ether"),
       },
       (error, transactionHash) => {
-				if (error) return console.log("Payment failed", error);
-				const etherscanLink = getEtherscanLink(network, transactionHash)
-				console.log(etherscanLink)
+        if (error) return console.log("Payment failed", error);
+        const etherscanLink = getEtherscanLink(network, transactionHash);
+        console.log(etherscanLink);
       }
     );
   };
